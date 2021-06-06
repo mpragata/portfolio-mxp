ea0fbb16c71b2511e4b3412e4bc069cc310f024f
Marc Xavier Pragata <marcxavierpragata@gmail.com>
2021-06-07 00:29:21 +0800

    Finished Contact Section
    Added personal contact info and icons

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import "./contacts.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCopyright, faMobileAlt} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons'
+import {faFacebookSquare, faDiscord} from '@fortawesome/free-brands-svg-icons'
 
 export default function Contacts() {
     return (
         <div className="contacts" id="contacts">
-            Contacts
+            <h2>Contacts</h2>
+            <div className="container">
+                <div className="item">
+                    <div className="iconContainer">
+                        <FontAwesomeIcon className="icon" icon={faEnvelope}/>
+                    </div>
+                    <span>marcxavierpragata@gmail.com <br/> marcxavierpragata2@gmail.com</span>
+                </div>
+                <div className="item">
+                    <div className="iconContainer">
+                        <FontAwesomeIcon className="icon" icon={faMobileAlt}/>
+                    </div>
+                    <span>09452500376 <br/> 09984533258 </span>
+                </div>
+                <div className="item">
+                    <div className="iconContainer">
+                        <FontAwesomeIcon className="icon" icon={faFacebookSquare}/>
+                    </div>
+                    <span>Marc Xavier E. Pragata</span>
+                </div>
+                <div className="item">
+                    <div className="iconContainer">
+                        <FontAwesomeIcon className="icon" icon={faDiscord}/>
+                    </div>
+                    <span>Chupzz#2191</span>
+                </div>
+            </div>
+            <div className="lineContainer">
+                <hr className="line"/>
+            </div>
+            <div className="footer">
+                <p>
+                    Pragata, Marc Xavier
+                    <FontAwesomeIcon className="copyright"icon={faCopyright}/>
+                </p>
+            </div>
         </div>
     )
 }
